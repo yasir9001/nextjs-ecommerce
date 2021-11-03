92e854151ae8c36a445a047281af01a6af939a97
Yasir Hussain <yasir.hussain@cooperativecomputing.com>
2021-11-04 01:56:49 +0500

    initial setup and header setup

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import { Button, Input } from "antd";
+import React, { ReactElement } from "react";
+import Image from "next/image";
+
+import { SearchOutlined } from "@ant-design/icons";
+
+interface Props {}
+
+function Header({}: Props): ReactElement {
+  return (
+    <div className="flex py-4">
+      <div className="flex-1 flex items-center">
+        <div className="mr-14">
+          <Image alt="logo" height="32" width="100" src="/assets/logo.svg" />
+        </div>
+        <div>
+          <ul className="flex">
+            <li className="mr-6">
+              <a>Home</a>
+            </li>
+            <li className="mr-6">
+              <a>Pages</a>
+            </li>
+            <li className="mr-6">
+              <a>Products</a>
+            </li>
+            <li className="mr-6">
+              <a>Blogs</a>
+            </li>
+            <li className="mr-6">
+              <a>Shop</a>
+            </li>
+            <li className="mr-6">
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex">
+        <Input />
+        <Button type="primary" className="min-h-full">
+          <SearchOutlined height="2rem" width="2rem" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1600px",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
